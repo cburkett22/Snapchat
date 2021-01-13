4425874dd612f9b55dd4f471d691cdb229b86ab6
Corey Burkett <coreyburkett22@gmail.com>
2021-01-12 16:10:56 -0800

    Completed 10 sec timer for picture

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -33,7 +33,10 @@ function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
             <Avatar className="chat__avatar" src={profilePic} />
             <div className="chat__info">
                 <h4>{username}</h4>
-                <p>Tap to view - <ReactTimeAgo date={new Date(timestamp?.toDate()).toUTCString()} /></p>
+                <p>
+                    {!read && "Tap to view -"}{" "}
+                    <ReactTimeAgo date={new Date(timestamp?.toDate()).toUTCString()} />
+                </p>
             </div>
 
             {!read && <StopRoundedIcon className="chat__readIcon" />}
--- src/components/ChatView/ChatView.js
+++ src/components/ChatView/ChatView.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { selectSelectedImage } from '../../features/appSlice';
 import "./ChatView.css";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 function ChatView() {
     const selectedImage = useSelector(selectSelectedImage);
@@ -21,6 +22,27 @@ function ChatView() {
     return (
         <div className="chatView">
             <img src={selectedImage} onClick={exit} alt="Camera screenshot" />
+            <div className="chatView__timer">
+                <CountdownCircleTimer
+                    isPlaying
+                    duration={10}
+                    strokeWidth={6}
+                    size={50}
+                    colors={[
+                        ['#004777', 0.33],
+                        ['#F7B801', 0.33],
+                        ['#A30000', 0.33],
+                    ]}
+                >
+                    {({ remainingTime }) => {
+                        if (remainingTime === 0) {
+                            exit();
+                        }
+
+                        return remainingTime;
+                    }}
+                </CountdownCircleTimer>
+            </div>
         </div>
     )
 }
